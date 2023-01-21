31ab914f74d1e7f95999b533e052ccd1df90ef40
DOGAMO19 <danidogm19@gmail.com>
2023-01-21 17:24:54 -0600

    Actualizacion Buscador Google Autocomplete

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,11 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'; 
+import { REACT_APP_GOOGLE_MAPS_APIKEY } from "@env";
+import { SafeAreaView } from 'react-native-safe-area-context';
+import tw from 'tailwind-react-native-classnames';
+ 
 
 export const MapScreen = () => {
   return (
-    <View>
-      <Text>MapScreen</Text>
-    </View>
+      <SafeAreaView style={tw `bg-gray-100 h-full`}>
+      
+        <GooglePlacesAutocomplete 
+          placeholder='A donde vamos ahora...?'
+          styles={{
+            container:{
+              flex:0,
+              padding: 10,
+              borderRadius: 10
+            },
+            textInput:{
+              fontSize: 20,
+              fontWeight: '20'
+            }
+          }}
+          query={{
+            key: REACT_APP_GOOGLE_MAPS_APIKEY,
+            language: "es", 
+          }}
+          nearbyPlacesAPI='GooglePlacesSearch'
+          debounce={300}
+        />
+      </SafeAreaView>
+
   )
 }
 
